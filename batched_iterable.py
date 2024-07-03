0bbb6edb0cb05bba81d4ef2a097ef83652e77b7f
Jacob Zietek <xoqcpn@gmail.com>
2024-07-03 14:33:07 -0700

    Create batched_iterable.py

---
+++ batched_iterable.py
@@ -0,0 +1,10 @@
+from itertools import islice
+
+def batched(iterable, n):
+    "Batch data into lists of length n. The last batch may be shorter."
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    if n < 1:
+        raise ValueError('n must be at least one')
+    it = iter(iterable)
+    while (batch := list(islice(it, n))):
+        yield batch
